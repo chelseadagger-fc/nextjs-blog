48d6f2593aef71fd2d09f5669ec65ae3e4aae7f0
Daniel <daniel.aldridge@alumni.unc.edu>
2023-07-10 23:46:10 +0300

    installed remark library; used so to render markdown content

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,13 +2,14 @@ import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
-}
+    const postData = await getPostData(params.id);
+  
+    return {
+      props: {
+        postData,
+      },
+    };
+  }
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -26,6 +27,8 @@ export default function Post({ postData }) {
         {postData.id}
         <br />
         {postData.date}
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </Layout>
     );
   }
